3367272c7d3be2a97d09f7b0ef135c9fd14465aa
Thais Costa <thais1fc2002@gmail.com>
2025-06-17 21:57:09 -0300

    materia aula 1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+import CircleIcon from "./assets/CircleIcon.png";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity>
+        <Text style={styles.title}>
+          Bem vindo,Thais
+        </Text>
+        <Image source={CircleIcon} style={styles.image} />
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingTop: 60,
+    paddingHorizontal: 3,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  image: {
+    tintColor: "#f00",
+    width: 50,
+    height: 50
+  },
+});
